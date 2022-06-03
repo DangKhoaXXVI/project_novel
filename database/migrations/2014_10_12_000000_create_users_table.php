3e8171684d885f3a8e25d7633caf43e5ce8e19e2
DangKhoa <ndkhoaxxi@gmail.com>
2022-06-03 17:16:46 +0700

    migration improve 2

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,13 +15,20 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
-            $table->string('email')->unique()->nullable();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role')->default('member');
-            $table->timestamp('email_verified_at')->nullable();   
             $table->rememberToken();
             $table->timestamps();
+            // $table->id();
+            // $table->string('username');
+            // $table->string('email')->unique()->nullable();
+            // $table->string('password');
+            // $table->string('role')->default('member');
+            // $table->timestamp('email_verified_at')->nullable();   
+            // $table->rememberToken();
+            // $table->timestamps();
         });
     }
 
