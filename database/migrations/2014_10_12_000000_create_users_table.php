76d75751ecd5dbe635c19cf5585a97e2916940e5
DangKhoa <ndkhoaxxi@gmail.com>
2022-06-21 15:22:10 +0700

    fix migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken()->nullable();
-            $table->integer('role');
+            $table->integer('role')->default(0);
             $table->timestamps();
         });
     }
