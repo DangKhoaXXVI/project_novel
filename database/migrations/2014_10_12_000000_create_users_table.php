3091d236a9128580573e0e30cca39cd1e3f5b34f
DangKhoa <ndkhoaxxi@gmail.com>
2022-06-21 16:48:07 +0700

    user profile

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,10 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('avatar')->nullable();
             $table->string('email')->unique();
+            $table->string('birthday')->nullable();
+            $table->string('favorite')->nullable();
+            $table->string('cover')->nullable();
+            $table->string('about')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken()->nullable();
