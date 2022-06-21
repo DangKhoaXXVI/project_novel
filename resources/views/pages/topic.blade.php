2392b9d03fc8677961fc21d670a4f00c419a8a37
NDK <0306191431@caothang.edu.vn>
2022-06-21 16:24:17 +0700

    fix-theme

---
+++ resources/views/pages/topic.blade.php
@@ -0,0 +1,12 @@
+
+<div class="col-xs-12 col-sm-12 col-md-3">
+            <div class="tin-tuc">
+                <div class="b_title"><strong><i class="far fa-newspaper"></i> Tin Tức</strong></div>
+                <p><a title="" href="">
+                    Bài viết thử nghiệm... </a></p>
+                    <div class="note">
+                        <a href="">Xem Thêm &gt;&gt;</a>
+                    </div>
+            </div>
+        </div>
+
